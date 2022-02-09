29b2be5343304adac32abf82951a07d6d0488028
KlaytonFacre <klayton.facre@hotmail.com>
2022-02-09 09:48:53 -0300

    09FEV0948

--- portscan.c
+++ portscan.c
@@ -20,8 +20,11 @@
 int main(int argc, char const *argv[])
 {
   int open_ports_count = 0;
-  int low_port_range;   // In case of use a port range to scan
-  int upper_port_range; // In case of use a port range to scan
+  int low_port_range;                 // In case of use a port range to scan
+  int upper_port_range;               // In case of use a port range to scan
+  struct sockaddr_in target;          // Struct to hold an IPv4 address + port defined in in.h
+  memset(&target, sizeof(target), 0); // To zero out the struct target, to ensure there is only zeroes on that memory position
+  target.sin_family = AF_INET;
 
   // check for pscan <host> <low_port> <upper_port>
   switch (argc)
@@ -29,29 +32,34 @@ int main(int argc, char const *argv[])
   case 2: // In case of pscan <target>, set the default port range
     low_port_range = 1;
     upper_port_range = 1024;
+    if (isdigit(argv[1]))
+    {
+      inet_aton(argv[1], &target.sin_addr);
+    }
+    else
+    {
+      perror("Target: ");
+      exit(2);
+    }
     break;
   case 4: // In case of pscan <target> [low_range] [upper_range]
     low_port_range = atoi(argv[2]);
     upper_port_range = atoi(argv[3]);
-    break;
-  default:
-    print_usage();
-    exit(-1);
-  }
-
-  struct sockaddr_in target;          // Struct to hold an IPv4 address + port defined in in.h
-  memset(&target, sizeof(target), 0); // To zero out the struct target, to ensure there is only zeroes on that memory position
-  target.sin_family = AF_INET;
-
-  if(isdigit(argv[1]))
+    if(isdigit(argv[1]))
     {
-      inet_aton(argv[1], &target.sin_addr);     
+      inet_aton(argv[1], &target.sin_addr);
     }
     else
     {
-        perror("Hostname: ");
-        exit(2);
+      perror("Target: ");
+      exit(2);
     }
+    break;
+  default:
+    print_usage();
+    exit(1);
+  }
+
   /*
   struct hostent *name;                                       // To hold an Hostname info and resolve it to IPv4
   name = gethostbyname(argv[1]);                              // Resolve an domain name to IPv4
@@ -67,7 +75,7 @@ int main(int argc, char const *argv[])
 
   for (int index = low_port_range; index <= upper_port_range; ++index) // Actual scanning start
   {
-    target.sin_port = htons(index);                      // Set the port that is gonna be scanned right now
+    target.sin_port = htons(index); // Set the port that is gonna be scanned right now
 
     int stream_socket = socket(AF_INET, SOCK_STREAM, 0); // Create a socket for TCP/IP 4 connection
     if (stream_socket == -1)                             // Check for socket erros on the socket() call above
