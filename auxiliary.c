86b77c0914421e0e39131d0b1f49f2d48ffd6e44
KlaytonFacre <klayton.facre@hotmail.com>
2022-02-09 12:52:51 -0300

    09FEV1252

--- auxiliary.c
+++ auxiliary.c
@@ -3,10 +3,11 @@
 
 extern int print_usage(void)
 {
+  printf("Personal portscan (Klayton Facre - 2022)\n\n");
   printf("Usage: pscan <host> [lower_limit] [upper_limit]\n");
-  printf("<host>: Set the target, its mandatory!\n");
-  printf("[lower_limit]: Optional to set the start port of the scan range\n");
-  printf("[upper_limit]: Optional to set the end port of the scan range\n\n");
+  printf(" -<host>: Set the target (mandatory).\n");
+  printf(" -[lower_limit]: Optional to set the start port of the scan range\n");
+  printf(" -[upper_limit]: Optional to set the end port of the scan range\n\n");
   printf("pscan will automatically scan the first 1024 ports if not told otherwise.\n");
 
   return 0;
